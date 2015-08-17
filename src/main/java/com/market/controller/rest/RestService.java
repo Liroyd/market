f870c30ade028d4b3b5527bc3ee4512511128115
Liroyd <alex_liroyd@yahoo.com>
2015-08-17 17:02:38 +0300

    Add examples of JSON/XML to RestService

--- src/main/java/com/market/controller/rest/RestService.java
+++ src/main/java/com/market/controller/rest/RestService.java
@@ -1,21 +1,50 @@
 package com.market.controller.rest;
 
+import com.market.model.RestEntity;
+import com.market.model.Role;
+import com.market.model.User;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @RestController
-@RequestMapping("/rest/hello")
+@RequestMapping("/rest/")
 public class RestService {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String greeting() {
-        return "It's alive!";
+    @RequestMapping(value = "/user/{name}", method = RequestMethod.GET)
+    public User getUserJson() {
+        Role role = new Role("lol");
+        Set<Role> roles = new HashSet<>();
+        roles.add(role);
+        return new User("Name","Password", roles);
     }
 
     @RequestMapping(value = "/{name}", method = RequestMethod.GET)
-    public String greeting(@PathVariable String name) {
+    public String getGreeting(@PathVariable String name) {
         return "Hello "+name;
+    }
+
+    @RequestMapping(value = "/json", method = RequestMethod.GET, produces = "application/json")
+    public RestEntity getJsonRestEntity() {
+        return new RestEntity("Name", 100);
+    }
+
+    @RequestMapping(value = "/json/{name}", method = RequestMethod.GET, produces = "application/json")
+    public RestEntity getJsonRestEntity(@PathVariable String name) {
+        return new RestEntity(name, 100);
+    }
+
+    @RequestMapping(value = "/xml", method = RequestMethod.GET)
+    public RestEntity getXmlRestEntity() {
+        return new RestEntity("Name", 100);
+    }
+
+    @RequestMapping(value="/xml/{name}", method = RequestMethod.GET)
+    public RestEntity getXmlRestEntity(@PathVariable String name) {
+        return new RestEntity(name, 100);
     }
 }
---
+++ src/main/java/com/market/model/RestEntity.java
@@ -0,0 +1,37 @@
+package com.market.model;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "restEntity")
+public class RestEntity {
+
+    String name;
+    int quantity;
+
+    public RestEntity(String name, int quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+
+    public RestEntity() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @XmlElement
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    @XmlElement
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
